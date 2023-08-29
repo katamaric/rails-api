bb4ab86c9d3ad878807363de66ac5e85ed2bc427
katamaric <katherinehcao@gmail.com>
2023-08-29 14:02:45 +0200

    first push

---
+++ app/controllers/current_user_controller.rb
@@ -0,0 +1,6 @@
+class CurrentUserController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    render json: UserSerializer.new(current_user).serializeable_hash[:data][:attributes], status: :ok
+  end
+end
